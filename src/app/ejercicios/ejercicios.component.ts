f5b48eb20f52c7563153845b7bcc7f8b9cdaf332
Paco Gomez <p.gomez@geekshubs.com>
2020-04-01 18:03:26 +0200

    Componente Ejercicios en AppComponent

--- src/app/ejercicios/ejercicios.component.ts
+++ src/app/ejercicios/ejercicios.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjerciciosComponent implements OnInit {
 
+  ejercicio = 'Flexion';
+  
   constructor() { }
 
   ngOnInit(): void {
