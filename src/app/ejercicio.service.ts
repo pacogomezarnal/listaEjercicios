1f526c7e29a0c12000d0c64a35cec4d48610115f
Paco Gomez <p.gomez@geekshubs.com>
2020-04-07 09:06:15 +0200

    Proveedor datos

--- src/app/ejercicio.service.ts
+++ src/app/ejercicio.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {Ejercicio} from './ejercicio';
+import {EJERCICIOS} from './collection-ejercicios';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class EjercicioService {
 
   constructor() { }
+
+  getEjercicios():Ejercicio[]{
+    return EJERCICIOS;
+  }
 }
