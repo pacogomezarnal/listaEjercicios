25b0d53191896bee4659394b9344901440a0e39c
Paco Gomez <p.gomez@geekshubs.com>
2020-04-10 09:08:43 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EjerciciosComponent} from './ejercicios/ejercicios.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ejercicios',component:EjerciciosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
