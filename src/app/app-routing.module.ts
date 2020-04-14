af05dbb161cec2a1b6bc2a9ed699a3db78db9ad2
Paco Gomez <p.gomez@geekshubs.com>
2020-04-14 10:36:53 +0200

    Redirecciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {EjerciciosComponent} from './ejercicios/ejercicios.component'
+import {PanelComponent} from './panel/panel.component'
 
 const routes: Routes = [
-  {path:'ejercicios',component:EjerciciosComponent}
+  {path:'ejercicios',component:EjerciciosComponent},
+  {path:'panel',component:PanelComponent},
+  {path:'',redirectTo:'/panel',pathMatch:'full'}
 ];
 
 @NgModule({
