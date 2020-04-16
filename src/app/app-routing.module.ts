c21243203dcdfa59088865b4bfc897baf6064d80
Paco Gomez <p.gomez@geekshubs.com>
2020-04-16 08:09:32 +0200

    Parametros rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {EjerciciosComponent} from './ejercicios/ejercicios.component'
 import {PanelComponent} from './panel/panel.component'
+import {EjercicioDetalleComponent} from './ejercicio-detalle/ejercicio-detalle.component'
 
 const routes: Routes = [
   {path:'ejercicios',component:EjerciciosComponent},
+  {path:'detalle/:id',component:EjercicioDetalleComponent},
   {path:'panel',component:PanelComponent},
   {path:'',redirectTo:'/panel',pathMatch:'full'}
 ];
