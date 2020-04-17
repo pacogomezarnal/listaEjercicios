a54c1866d3f60489b57de92b0958905d13a8e610
Paco Gomez <p.gomez@geekshubs.com>
2020-04-17 08:19:49 +0200

    ActivatedRoute

--- src/app/ejercicio-detalle/ejercicio-detalle.component.ts
+++ src/app/ejercicio-detalle/ejercicio-detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ejercicio } from '../ejercicio';
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-ejercicio-detalle',
@@ -10,9 +11,14 @@ export class EjercicioDetalleComponent implements OnInit {
 
   @Input() ejercicio:Ejercicio;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getEjercicio;
+  }
+
+  getEjercicio():void{
+    const id=+this.route.snapshot.paramMap.get('id');
   }
 
 }
